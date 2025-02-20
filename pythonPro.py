bc5f13aac3c27d43a392c87fdf9c0e1bad34c688
reethuo <152209084+reethuo@users.noreply.github.com>
2025-02-20 16:42:33 +0530

    Update pythonPro.py

--- pythonPro.py
+++ pythonPro.py
@@ -1,5 +1,5 @@
-def fu(*args,**kwargs):
-    print('positional args: ',args)
+def fun(*args,**kwargs):
+    prin('positional args: ',args)
     print('keyword args: ',kwargs)
     print(type(args))
 fun(5,2,a=3,b=4)
